bacfa06f039c0749267e0b93d9460fafe8c1eee1
Alessandro Barreto <alessandrobarreto50@gmail.com>
2021-02-20 03:57:04 -0400

    Update decrypt_secret.sh

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -2,5 +2,5 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$ALESSANDRO" \
+gpg --quiet --batch --yes --decrypt --passphrase="$ALE" \
 --output test.txt test.txt.gpg
