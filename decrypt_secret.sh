b3688703d2aba8812f068a6b0aa2f26598cca67a
Alessandro Barreto <alessandrobarreto50@gmail.com>
2021-02-20 03:44:20 -0400

    Update decrypt_secret.sh

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/test.json test.txt.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$ALESSANDRO" \
+--output test.txt test.txt.gpg
